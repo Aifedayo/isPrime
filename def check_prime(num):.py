574aeb25b88b4ef40abf4cc86e4b021aac492b94
Aifedayo <akeemifedayolag@gmail.com>
2021-10-15 17:57:45 +0100

    First

---
+++ def check_prime(num):.py
@@ -0,0 +1,20 @@
+def check_prime(num):
+    prime_checker = False
+
+    if num == 0:
+        prime_checker = False
+    elif num == 1 and num <=2:
+        prime_checker = True
+    if num > 1:
+        for i in range(2, num):
+            if (num % i == 0):
+                prime_checker = True
+                break
+
+    if prime_checker:
+        print(f'{num} is not a prime number')
+    else:
+        print(f'{num} is  a prime number')
+
+check_prime(12)
+            
